8b6e568217969c4ae2922860fbde73090c921180
ryoya <sateraito3696@gmail.com>
2025-06-19 15:04:07 +0900

    lexicalの追加とEditorコンポーネントファイルの追加

--- src/components/MemoCard.tsx
+++ src/components/MemoCard.tsx
@@ -1,9 +1,10 @@
+import { EditorState } from "lexical";
 import { useState } from "react"
 
 export type MemoCardInfo = {
   cardId: string,
   title: string,
-  value: string,
+  value: EditorState,
   posX: number,
   posY: number
   width: number,
@@ -16,6 +17,8 @@ export default function MemoCard(memoCardInfo: MemoCardInfo): JSX.Element {
   const [posX, setPosX] = useState<number>(memoCardInfo.posX)
   const [posY, setPosY] = useState<number>(memoCardInfo.posY)
   return(
-    <div style={{width: cardWidth, height: cardHeight, top: posX, left: posY}}></div>
+    <div style={{width: cardWidth, height: cardHeight, top: posX, left: posY}}>
+      
+    </div>
   )
 }
---
+++ src/components/lexicalEditor/Editor.tsx
@@ -0,0 +1,36 @@
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { EditorState } from "lexical";
+
+const thme = {} // 後で別ファイルに分離
+function onError(error: any) {
+  console.log(error);
+} 
+export default function Editor(editorState: EditorState) {
+  const initialConfig = {
+    namespace: 'editor',
+    thme,
+    onError,
+    editorState
+  }
+
+  return(
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={
+          <ContentEditable
+            aria-placeholder="ここに入力..."
+            placeholder={<div>ここに入力...</div>}
+          />
+        }
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  )
+}
