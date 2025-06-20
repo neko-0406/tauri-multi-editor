6d083bebca51eba9b17623786b04d549c820290e
neko-04 <sateraito3696@gmail.com>
2025-06-21 08:27:58 +0900

    Editorにプラグイン追加

--- src/components/lexicalEditor/Editor.tsx
+++ src/components/lexicalEditor/Editor.tsx
@@ -8,6 +8,15 @@ import { EditorState } from 'lexical';
 
 import editorTheme from './EditorTheme';
 import editorNodes from './EditorNode';
+import { CharacterLimitPlugin } from '@lexical/react/LexicalCharacterLimitPlugin';
+import { ClickableLinkPlugin } from '@lexical/react/LexicalClickableLinkPlugin';
+import { HashtagPlugin } from '@lexical/react/LexicalHashtagPlugin';
+import { HorizontalRulePlugin } from '@lexical/react/LexicalHorizontalRulePlugin';
+import { ListPlugin } from '@lexical/react/LexicalListPlugin';
+import { TabIndentationPlugin } from '@lexical/react/LexicalTabIndentationPlugin';
+import { TablePlugin } from '@lexical/react/LexicalTablePlugin';
+import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
+import { CheckListPlugin } from '@lexical/react/LexicalCheckListPlugin';
 
 function onError(error: any) {
   console.log(error);
@@ -29,6 +38,16 @@ export default function Editor(editorState: EditorState) {
       />
       <HistoryPlugin />
       <AutoFocusPlugin />
+      <CharacterLimitPlugin charset={'UTF-8'} maxLength={Number.MAX_SAFE_INTEGER} />
+      <ClickableLinkPlugin />
+      <HashtagPlugin />
+      <HorizontalRulePlugin />
+      <ListPlugin />
+      <TabIndentationPlugin maxIndent={10} />
+      <TablePlugin hasCellMerge={true} hasCellBackgroundColor={true} hasHorizontalScroll={true} />
+      <LinkPlugin />
+      <CheckListPlugin />
+
     </LexicalComposer>
   );
 }
