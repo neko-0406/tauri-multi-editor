d6c120ac016b4038dfd6f33af6b6805043812910
neko-04 <sateraito3696@gmail.com>
2025-06-22 07:34:10 +0900

    選択されたタブを白、それ以外を明るめのグレーになるように変更

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -14,6 +14,7 @@ export type TabItemData = {
 // TabItemTagsの引数
 type TabItemTagsProps = {
   tabItems: TabItemData[];
+  selectedId: string | null;
   updateSelectedId: React.Dispatch<React.SetStateAction<string | null>>;
 };
 
@@ -22,9 +23,14 @@ type TabContainerProps = {
   tabItems: TabItemData[];
 };
 
-export function TabItemTags({ tabItems, updateSelectedId }: TabItemTagsProps) {
+export function TabItemTags({ tabItems, selectedId, updateSelectedId }: TabItemTagsProps) {
   return tabItems.map((item) => (
-    <div key={item.id} onClick={() => updateSelectedId(item.id)}>
+    <div
+      key={item.id}
+      className="tab-item-tag"
+      style={{ backgroundColor: selectedId === item.id ? 'white' : '#dcdcdc' }}
+      onClick={() => updateSelectedId(item.id)}
+    >
       {item.title}
     </div>
   ));
@@ -37,12 +43,10 @@ export function TabContainer({ tabItems }: TabContainerProps) {
     <div className="tab-space">
       {/* タブのタグ部分を置くところ */}
       <div className="tab-tag-container">
-        <TabItemTags tabItems={tabItems} updateSelectedId={setSelectedId} />
+        <TabItemTags tabItems={tabItems} selectedId={selectedId} updateSelectedId={setSelectedId} />
       </div>
       {/* 選択されたタブの内容を表示するところ */}
-      <div className="tab-display-space">
-        {tabItems.filter((item) => selectedId === item.id).map((item) => item.components.value)}
-      </div>
+      <div className="tab-display-space">{tabItems.filter((item) => selectedId === item.id)[0].components.value}</div>
     </div>
   );
 }
