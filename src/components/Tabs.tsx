451a875d4ce4a8c1ce063a6c43f755edd7bc7dd8
neko-04 <sateraito3696@gmail.com>
2025-06-22 09:35:21 +0900

    タブのタグ部分にボタン表示を追加

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { FaXmark } from 'react-icons/fa6';
 import '../styles/Tabs.css';
 
 export type TabItemData = {
@@ -9,6 +10,13 @@ export type TabItemData = {
     value: string;
     path: string;
   };
+};
+
+// TabItemの引数
+type TabItemProps = {
+  tabItemData: TabItemData;
+  selectedId: string | null;
+  updateSelectedId: React.Dispatch<React.SetStateAction<string | null>>;
 };
 
 // TabItemTagsの引数
@@ -23,16 +31,41 @@ type TabContainerProps = {
   tabItems: TabItemData[];
 };
 
+export function TabItem({ tabItemData, selectedId, updateSelectedId }: TabItemProps) {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
+  const handleMouseEnter = useCallback(() => {
+    setIsHover(true);
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    setIsHover(false);
+  }, []);
+
+  return (
+    <div
+      key={tabItemData.id}
+      className="tab-item-tag"
+      style={{ backgroundColor: selectedId === tabItemData.id ? 'white' : '#dcdcdc' }}
+      onClick={() => updateSelectedId(tabItemData.id)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="tab-item-tag-title-space">{tabItemData.title}</div>
+      <div className="tab-item-tag-icon-space">
+        {isHover ? (
+          <button type="button" className="tab-item-tag-remove">
+            <FaXmark size={15} />
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
 export function TabItemTags({ tabItems, selectedId, updateSelectedId }: TabItemTagsProps) {
   return tabItems.map((item) => (
-    <div
-      key={item.id}
-      className="tab-item-tag"
-      style={{ backgroundColor: selectedId === item.id ? 'white' : '#dcdcdc' }}
-      onClick={() => updateSelectedId(item.id)}
-    >
-      {item.title}
-    </div>
+    <TabItem tabItemData={item} selectedId={selectedId} updateSelectedId={updateSelectedId} />
   ));
 }
 
@@ -46,7 +79,7 @@ export function TabContainer({ tabItems }: TabContainerProps) {
         <TabItemTags tabItems={tabItems} selectedId={selectedId} updateSelectedId={setSelectedId} />
       </div>
       {/* 選択されたタブの内容を表示するところ */}
-      <div className="tab-display-space">{tabItems.filter((item) => selectedId === item.id)[0].components.value}</div>
+      <div className="tab-display-space">{tabItems.find((item) => selectedId === item.id)?.components.value}</div>
     </div>
   );
 }
