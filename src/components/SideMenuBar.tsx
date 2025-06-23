65b8c0d6c64f5eedc05bb156d238cccf5f12abd8
ryoya <sateraito3696@gmail.com>
2025-06-23 18:26:09 +0900

    SideMenuBarコンポーネントとスタイルを追加

---
+++ src/components/SideMenuBar.tsx
@@ -0,0 +1,12 @@
+export default function SideMenuBar() {
+  return(
+    <div className="side-menu-bar-container">
+      {/* 検索などを入れるところ */}
+      <div className="side-menu-top-icon-container">
+        
+      </div>
+      {/* 設定などを入れるとこ */}
+      <div className="side-menu-bottom-icon-container"></div>
+    </div>
+  )
+}
