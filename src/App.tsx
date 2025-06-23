0675df0a8c4349fa7751c75b51e9373e5583f6bb
Ryoya Otomo <s2212214@st.tohtech.ac.jp>
2025-06-23 20:04:26 +0900

    デモデータの追加とか

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { AppSettingProvider } from './components/AppSetting/SettingProvider';
 import { AppStateProvider } from './components/AppState/StateProvider';
 import SideMenuSpace from './components/SideMenuSpace';
 import { TabContainer, TabItemData } from './components/Tabs';
-import SideMenuBar from './components/SideMenuBar';
+import SideMenuBar, { SideMenuItem } from './components/SideMenuBar';
+import { FaGear } from 'react-icons/fa6';
+
+const demoData: SideMenuItem = {
+  label: 'gear',
+  icon: <FaGear size={30} />,
+  click: () => console.log('gear!!'),
+};
 
 const tabData: TabItemData[] = [
   {
@@ -33,7 +40,7 @@ function App() {
         <div className="main-container">
           {/* サイドメニューバー */}
           <div className="side-menu-bar">
-            <SideMenuBar />
+            <SideMenuBar sideMenuItems={[demoData]} />
           </div>
           {/* サイドメニューバー関連のUI置くとこ・基本閉じる*/}
           <SideMenuSpace />
