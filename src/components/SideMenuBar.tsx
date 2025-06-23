8ca255076320cf6793bc687617bd155190ada332
Ryoya Otomo <s2212214@st.tohtech.ac.jp>
2025-06-23 20:05:02 +0900

    データからアイコンボタンの表示まで

--- src/components/SideMenuBar.tsx
+++ src/components/SideMenuBar.tsx
@@ -1,20 +1,34 @@
-import { FaGear } from 'react-icons/fa6';
+import '../styles/SideMenuBar.css';
 
-export default function SideMenuBar() {
+export type SideMenuItem = {
+  label: string;
+  icon: JSX.Element;
+  click: () => void;
+};
+
+type SideMenuBarProps = {
+  sideMenuItems: SideMenuItem[]
+}
+
+export default function SideMenuBar({ sideMenuItems }: SideMenuBarProps) {
   return (
     <div className="side-menu-bar-container">
       {/* 検索などを入れるところ */}
       <div className="side-menu-top-icon-container"></div>
       {/* 設定などを入れるとこ */}
-      <div className="side-menu-bottom-icon-container"></div>
+      <div className="side-menu-bottom-icon-container">
+        {sideMenuItems.map((item) => (
+          <SideMenuItemIcom label={item.label} icon={item.icon} click={item.click} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export function SideMenuItemIcom() {
+export function SideMenuItemIcom({ label, icon, click }: SideMenuItem) {
   return (
-    <button className="side-menu-button-icon" type="button">
-      <FaGear size={30} />
+    <button className="side-menu-button-icon" type="button" onClick={click}>
+      {icon}
     </button>
   );
 }
