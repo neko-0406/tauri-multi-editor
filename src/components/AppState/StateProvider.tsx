045d4b437721bb588cea9c07917bafb663a3ff7a
Ryoya Otomo <s2212214@st.tohtech.ac.jp>
2025-06-24 20:25:06 +0900

    デモデータで初期化

--- src/components/AppState/StateProvider.tsx
+++ src/components/AppState/StateProvider.tsx
@@ -1,17 +1,38 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { FileItemData } from '../Tabs';
 
 export type AppStateType = {
   openWorkspace: string;
+  selectedTabId: string;
   openFiles: FileItemData[];
   sideMenuSpaceWidth: number;
 };
 
 const defaultAppSetting: AppStateType = {
   openWorkspace: '',
+  selectedTabId: '',
   openFiles: [],
   sideMenuSpaceWidth: 0,
 };
+
+const filesData: FileItemData[] = [
+  {
+    id: 'test-1',
+    title: 'test-title1',
+    components: {
+      type: 'md',
+      path: './test1.md',
+    },
+  },
+  {
+    id: 'test-2',
+    title: 'test-title2',
+    components: {
+      type: 'md',
+      path: './test2.md',
+    },
+  },
+];
 
 const AppStateContext = createContext<{
   appState: AppStateType;
@@ -23,6 +44,10 @@ const AppStateContext = createContext<{
 
 export const AppStateProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
   const [appState, setAppState] = useState<AppStateType>(defaultAppSetting);
+  
+  useEffect(() => {
+    setAppState(state => ({ ...state, openFiles: filesData }));
+  }, []);
 
   return <AppStateContext.Provider value={{ appState, setAppState }}>{children}</AppStateContext.Provider>;
 };
