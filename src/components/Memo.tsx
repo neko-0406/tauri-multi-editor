c807ac799292eb0ff4a66c81dfc0714e57fcea7a
neko-04 <sateraito3696@gmail.com>
2025-07-04 23:04:46 +0900

    Memoコンポーネントの構造を追加し、位置とサイズを管理するための状態を実装。MemoBoardコンポーネントを更新してMemoを表示。

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -1,7 +1,42 @@
-export default function Memo() {
+import Editor from './lexicalEditor/Editor';
+
+import '../styles/Memo.css';
+import { useState } from 'react';
+
+interface MemoProps {}
+interface MemoBoardProps {}
+
+type MemoPosition = { x: number; y: number };
+type MemoSize = { w: number; h: number };
+
+export function Memo({}: MemoProps) {
+  const [memoPosition, setMemopostion] = useState<MemoPosition>({x:50, y:50});
+  const [memoSize, setMemoSize] = useState<MemoSize>({w:150, h:110});
+
+  
   return (
-    <div className="memo-space">
-      
+    <div
+      className="memo"
+      style={{
+        top: `${memoPosition.x}px`,
+        left: `${memoPosition.y}px`,
+        width: `${memoSize.w}px`,
+        height: `${memoSize.h}px`,
+      }}
+    >
+      <div className="memo-title-space"></div>
+      <div className="markdown-space">
+        <Editor />
+      </div>
+      <div className="memo-resize-area" />
     </div>
-  )
+  );
+}
+
+export default function MemoBoard({}: MemoBoardProps) {
+  return (
+    <div className="memo-board-space">
+      <Memo />
+    </div>
+  );
 }
