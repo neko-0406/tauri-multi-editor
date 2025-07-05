7a3a7a800c4ad8f9afc77326e4efbf71146df853
neko-04 <sateraito3696@gmail.com>
2025-07-05 09:27:41 +0900

    Memoコンポーネントに編集機能を追加し、状態管理を改善

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -1,7 +1,9 @@
 import Editor from './lexicalEditor/Editor';
 
 import '../styles/Memo.css';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { AiOutlineRead } from 'react-icons/ai';
+import { AiOutlineEdit } from 'react-icons/ai';
 
 interface MemoProps {}
 interface MemoBoardProps {}
@@ -10,10 +12,12 @@ type MemoPosition = { x: number; y: number };
 type MemoSize = { w: number; h: number };
 
 export function Memo({}: MemoProps) {
-  const [memoPosition, setMemopostion] = useState<MemoPosition>({x:50, y:50});
+  const [memoPosition, setMemoPostion] = useState<MemoPosition>({x:50, y:50});
   const [memoSize, setMemoSize] = useState<MemoSize>({w:150, h:110});
+  const [isEdit, setIsEdit] = useState<boolean>(false);
 
-  
+  const handleIsEditClick = useCallback(() => setIsEdit(pre => !pre), [])
+
   return (
     <div
       className="memo"
@@ -21,14 +25,17 @@ export function Memo({}: MemoProps) {
         top: `${memoPosition.x}px`,
         left: `${memoPosition.y}px`,
         width: `${memoSize.w}px`,
-        height: `${memoSize.h}px`,
+        // height: `${memoSize.h}px`,
       }}
     >
-      <div className="memo-title-space"></div>
+      <div className="memo-icon-space">
+        <button type='button' className='memo-tool-button' onClick={handleIsEditClick}>{isEdit ? <AiOutlineEdit size={20}/> : <AiOutlineRead size={20}/> }</button>
+      </div>
       <div className="markdown-space">
         <Editor />
       </div>
       <div className="memo-resize-area" />
+
     </div>
   );
 }
